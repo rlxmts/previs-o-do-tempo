7de96a5e6abaafa74ac8ececb3c90f7d4074ea3f
Matheus Cruz <165737916+rlxmts@users.noreply.github.com>
2024-08-31 14:35:09 -0300

    :bug: Bug: removendo erro ao busca bem sucedida

--- src/Context/buscaApiContext.jsx
+++ src/Context/buscaApiContext.jsx
@@ -18,6 +18,7 @@ export const BuscaApiProvider = ({children})=> {
       const res = await axios.get(`https://api.openweathermap.org/data/2.5/forecast?q=${cidadeDigitada}&lang=pt_br&cnt=3&appid=${key}`);
       setDados(res.data.list);
       setCidade(res.data.city.name);
+      setErro(null);
     }catch(erro){
       setErro(erro);
     }finally{
